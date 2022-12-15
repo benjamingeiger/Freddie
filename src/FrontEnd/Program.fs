5a59e85dff7d23b0f290eab3becb073223be7ad5
Benjamin Geiger <github@bgeiger.net>
2022-12-14 21:01:16 -0500

    Baby steps.

--- src/FrontEnd/Program.fs
+++ src/FrontEnd/Program.fs
@@ -1,2 +1,10 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+open Interpreter
+
+[<EntryPoint>]
+let main argv =
+    let result = Parser.parse ["foo"]
+    printfn "%A" result
+
+    0
---
+++ src/Interpreter/Parser.fs
@@ -0,0 +1,11 @@
+﻿namespace Interpreter
+
+module Parser =
+    open FParsec
+
+    let parseProgram = pstring "foo"
+
+    let parse lines =
+        lines
+        |> String.concat "\n"
+        |> runParserOnString parseProgram () ""
